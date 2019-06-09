6e94cceaa773f189d87ba4934bd2ec7ac9ae00ba
liaobifeng <845724731@qq.com>
2019-06-09 15:46:17 +0800

    Initial commit

---
+++ src/main/java/com/qianfeng/springboot_git/controller/OurController.java
@@ -0,0 +1,14 @@
+package com.qianfeng.springboot_git.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @auther liaobifeng
+ * @Date 2019/6/9
+ */
+@Controller
+@RequestMapping("our")
+public class OurController {
+
+}
